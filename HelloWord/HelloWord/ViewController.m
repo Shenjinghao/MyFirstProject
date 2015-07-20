bf3333e8b94880963229490b2bae89f91407b577
Shenjinghao <Shenjinghao@lanou3gdeMac-mini-5.local>
2015-07-20 14:57:19 +0800

    111

---
+++ HelloWord/HelloWord/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloWord
+//
+//  Created by 申景灏 on 15/7/20.
+//  Copyright (c) 2015年 myProject. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
